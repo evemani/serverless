a0039dac4c5219b4092257abf519211000199431
Manish Verma <manish.verma@intelematics.com>
2018-11-12 15:08:43 +0530

    failure message

--- api/CICD/src/main/java/com/lambda/DemoHandler.java
+++ api/CICD/src/main/java/com/lambda/DemoHandler.java
@@ -9,7 +9,7 @@ public class DemoHandler implements RequestHandler<ServerlessInput, GatewayRespo
 	@Override
 	public GatewayResponse handleRequest(ServerlessInput input, Context context) {
 		// TODO Auto-generated method stub
-		return new GatewayResponse(200, "verma help2", GatewayResponse.HEADERS_JSON);
+		return new GatewayResponse(200, "verma help2", GatewayResponse.HEADERS_JSON)
 	}
 
 }
